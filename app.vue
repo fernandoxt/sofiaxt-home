e6f43918c4645e0f6eeb70ca287b851d7a5c5f95
Fernando Herrera <fernando.herrera@sofiaxt.com>
2022-11-03 22:37:23 -0700

    add vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <v-card text="Vuetify" variant="tonal">
+        <h1>Welcome to the homepage</h1>
+        <v-menu transition="slide-x-transition">
+            <template v-slot:activator="{ props }">
+                <v-btn
+                        color="primary"
+                        class="ma-2"
+                        v-bind="props"
+                >
+                    Slide X Transition
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item
+                        v-for="n in 5"
+                        :key="n"
+                        link
+                >
+                    <v-list-item-title v-text="'Item ' + n"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </v-card>
 </template>
